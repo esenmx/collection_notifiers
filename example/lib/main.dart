c5c0913cddce0a6be0e0e94bded800500df246d2
softronaut <mathronaut@gmail.com>
2022-02-27 19:12:51 +0300

    example format fix

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -18,11 +18,11 @@ class App extends StatelessWidget {
 }
 
 final setNotifier =
-ChangeNotifierProvider.autoDispose((ref) => SetNotifier<int>([1, 1, 1]));
+    ChangeNotifierProvider.autoDispose((ref) => SetNotifier<int>([1, 1, 1]));
 final listProvider =
-ChangeNotifierProvider((ref) => ListNotifier<int>([1, 2, 3]));
+    ChangeNotifierProvider((ref) => ListNotifier<int>([1, 2, 3]));
 final mapProvider =
-ChangeNotifierProvider((ref) => MapNotifier<String, int>({'a': 1}));
+    ChangeNotifierProvider((ref) => MapNotifier<String, int>({'a': 1}));
 
 class Home extends ConsumerWidget {
   const Home({Key? key}) : super(key: key);
